d13c326b36b7a562200ae3e3c0d9b4f2a1e3c4d1
jah09 <jasuhot1@gmail.com>
2024-05-12 18:41:25 +0800

    implemented CRUD

--- base/views.py
+++ base/views.py
@@ -3,44 +3,65 @@ from .models import Todo
 # Create your views here.
 
 
-def index(request):
+def home(request):
     todos = Todo.objects.all()
     if request.method == 'POST':
         title = request.POST.get('title')
         description = request.POST.get('description')
 
         Todo.objects.create(title=title, description=description)
- 
 
-    context = {   'todos': todos}
-    return render(request, 'base/index.html', context)
+    context = {'todos': todos}
+    return render(request, 'base/home.html', context)
 
 
-# def create_todo(request):
-#     if request.method == 'POST':
-#         title = request.POST.get('title')
-#         description = request.POST.get('description')
-#         print(title, description)
-#         Todo.objects.create(title=title, description=description)
-#         return redirect('index.html')
-#
-#
-#     context = {}
-#     return render(request, 'base/index.html', context)
+def create_todo(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+
+        Todo.objects.create(title=title, description=description)
+
+        return redirect('home')
+
+    # context = {}
+    return render(request, 'base/todo_form.html')
 
 
 def todos(request):
 
     todos = Todo.objects.all()
-
+    print(todos)
     context = {
         'todos': todos
     }
-    return render(request, 'base/index.html', context)
+    return render(request, 'base/home.html', context)
 
 
 def delete_todos(request, pk):
     todo = Todo.objects.get(id=pk)
     if request.method == 'POST':
         todo.delete()
-        return redirect(request, 'base/index.html')
+        return redirect('home')
+
+
+def edit_todo(request, pk):
+    page='edit'
+    todo = Todo.objects.get(id=pk)
+    print("title", todo.title)
+    if request.method == 'POST':
+
+        todo.title = request.POST.get('title')
+        todo.description = request.POST.get('description')
+        todo.save()
+        return redirect('home')
+
+    context = {'todo': todo, 'page': page}
+
+  
+    return render(request, 'base/todo_form.html',context)
+
+
+    
+  
+  
